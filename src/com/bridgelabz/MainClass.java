0d473e7dbcbdc27ad72ce0ecc29abf47f882da7d
AdeshMask <adeshmaske255@gmail.com>
2022-03-28 22:40:20 +0530

    Welcome to LinkedList

---
+++ src/com/bridgelabz/MainClass.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("************Linked List**********");
+        LinkedList list = new LinkedList();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        list.display();
+    }
+}
